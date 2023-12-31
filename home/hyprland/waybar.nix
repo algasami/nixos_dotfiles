b680ab8dd5dfc1e1b13c2b5ae9ca38b0f1668112
algasami <105358814+algasami@users.noreply.github.com>
2023-12-31 20:46:37 +0800

    waybar config

--- home/hyprland/waybar.nix
+++ home/hyprland/waybar.nix
@@ -5,14 +5,17 @@
     systemd.enable = true; # run on start
     style = ''
       ${builtins.readFile "${./macchiato.css}"}
+      * { font-size: 20px; font-family: Iosevka Nerd Font; }
     '';
     settings = [{
-      height = 30;
+      height = 40;
       layer = "top";
       position = "bottom";
       tray = { spacing = 10; };
       modules-center = [];
-      modules-left = [];
+      modules-left = [
+        "clock"
+      ];
       modules-right = [
         "network"
         "cpu"
@@ -20,25 +23,24 @@
         "temperature"
       ];
       clock = {
-        format-alt = "{:%Y-%m-%d}";
-        tooltip-format = "{:%Y-%m-%d | %H:%M}";
+        format = " 󱑔 {:%Y-%m-%d | %H:%M} ";
       };
       cpu = {
-        format = "{usage}%   ";
+        format = "{usage}%   | ";
         tooltip = false;
       };
       memory = { format = "{}%   "; };
       network = {
         interval = 1;
-        format-alt = "{ifname}: {ipaddr}/{cidr}";
-        format-disconnected = "Disconnected ⚠  ";
-        format-ethernet = "{ifname}: {ipaddr}/{cidr}   up: {bandwidthUpBits} down: {bandwidthDownBits}";
-        format-linked = "{ifname} (No IP)";
-        format-wifi = "{essid} ({signalStrength}%)   ";
+        format-alt = "{ifname}: {ipaddr}/{cidr} ";
+        format-disconnected = "Disconnected 󰲜  | ";
+        format-ethernet = "{ifname}: {ipaddr}/{cidr} 󰱔  | 󰕒 {bandwidthUpBits} 󰇚 {bandwidthDownBits} | ";
+        format-linked = "{ifname} (No IP) ";
+        format-wifi = "{essid} ({signalStrength}%)   | ";
       };
       temperature = {
         critical-threshold = 80;
-        format = "{temperatureC}°C {icon}";
+        format = "{temperatureC}°C {icon} ";
         format-icons = [ " " " " " " ];
       };
     }];
