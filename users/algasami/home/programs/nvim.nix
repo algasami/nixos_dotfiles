aff142b38744d6ef4c811cc1321f1ec4c0588c91
algasami <105358814+algasami@users.noreply.github.com>
2024-02-09 10:06:50 +0800

    nvim update

--- users/algasami/home/programs/nvim.nix
+++ users/algasami/home/programs/nvim.nix
@@ -1,18 +1,56 @@
 { config, pkgs, ...}:
 {
+  home.packages = with pkgs; [
+    ccls # lsp for c,c++
+  ];
   programs.neovim = {
     enable = true;
     viAlias = true;
     vimAlias = true;
-    extraConfig = ''
-      set relativenumber number
-      set tabstop=2
-      set shiftwidth=2
-      set expandtab
-      set softtabstop=2
+    extraLuaConfig = ''
+      o = vim.o
+      g = vim.g
+      wo = vim.wo
+      bo = vim.bo
+
+      o.showcmd = true
+      g.mapleader = ","
+
+      wo.number = true
+      wo.relativenumber = true;
+
+      bo.tabstop = 2;
+      bo.shiftwidth = 2;
+      bo.expandtab = true;
+      bo.softtabstop = 2;
+
+      vim.keymap.set("n", "<Leader>t", "<cmd>Neotree toggle<cr>")
     '';
-    plugins = [
-      pkgs.vimPlugins.suda-vim
+    coc = {
+      enable = true;
+      settings = {
+        languageserver= {
+          ccls= {
+            command = "ccls";
+            filetypes= ["c" "cc" "cpp" "c++" "objc" "objcpp"];
+            rootPatterns= [".ccls" "compile_commands.json" ".git/" ".hg/"];
+            initializationOptions= {
+              cache= {
+                directory= "/tmp/ccls";
+              };
+            };
+          };
+        };
+      };
+    };
+    plugins = with pkgs.vimPlugins; [
+      suda-vim # sudo
+      vim-nix # nix highlight
+      nvim-treesitter # syntax
+      nvim-web-devicons # icons
+      nui-nvim # img preview
+      plenary-nvim # library
+      neo-tree-nvim # tree
     ];
   };
 }
